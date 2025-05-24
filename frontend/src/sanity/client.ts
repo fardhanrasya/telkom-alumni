9ece47887ca9b60da285157696fd067116a4f097
fardhanrasya <fardhanhafist14@gmail.com>
2025-05-24 23:03:53 +0700

    update client dataset to dev

--- frontend/src/sanity/client.ts
+++ frontend/src/sanity/client.ts
@@ -2,7 +2,7 @@ import { createClient } from "next-sanity";
 
 export const client = createClient({
   projectId: "1btnolup",
-  dataset: "production",
+  dataset: "dev",
   apiVersion: "2024-01-01",
   useCdn: false,
 });
